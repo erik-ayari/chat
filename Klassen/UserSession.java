49ebd32475f2390041f8b8d3ce1a54a277105f44
xerayx <39841313+xerayx@users.noreply.github.com>
2018-06-26 22:46:23 +0200

    Wait For Message

--- Klassen/UserSession.java
+++ Klassen/UserSession.java
@@ -1,18 +1,26 @@
 package methods;
 
-
 import methods.exceptions.InvalidLoginArguments;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.awt.BorderLayout;
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Iterator;
+
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class UserSession {
 
@@ -46,7 +54,7 @@ public class UserSession {
     private boolean checkIdentity(String username, String password) throws JSONException, IOException {
 
 
-        String pfad = "C:/Users/Niels/eclipse-workspace/Schule Chat/src/methods/users.json";
+        String pfad = "C:/Users/Erik/eclipse-workspace/Client/src/methods/users.json";
         File file = new File(pfad);
 
         String content = new String(Files.readAllBytes(Paths.get(file.toURI())), "UTF-8");
@@ -88,6 +96,57 @@ public class UserSession {
         }
 
 
+    }
+    
+    public void waitForMessage(ArrayList<JPanel> panels, JTabbedPane tabbedPane) {
+    	try {
+            ServerSocket serverSocket = new ServerSocket(this.serverPort);
+
+            //Eine Verbindung wird eingegangen
+            Socket socket = serverSocket.accept();
+
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+            String[] input = (String[]) in.readObject();
+
+
+            String art = input[0];
+            String message = input[1];
+            String channel = input[2];
+
+            serverSocket.close();
+
+            switch (art) {
+                case "msg":
+                	int index = findTabByName(channel, tabbedPane);
+                	BorderLayout layout = (BorderLayout) panels.get(index).getLayout();
+    				JTextArea chatHistory = (JTextArea) layout.getLayoutComponent(BorderLayout.CENTER);
+    				chatHistory.append("\n" + message);
+                    //return new String[]{message, channel};
+
+                case "login":
+                    break;
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    public int findTabByName(String title, JTabbedPane tab)  
+    {
+      int tabCount = tab.getTabCount();
+      for (int i=0; i < tabCount; i++) 
+      {
+        String tabTitle = tab.getTitleAt(i);
+        if (tabTitle.equals(title)) return i;
+      }
+      return -1;
     }
 
 }
