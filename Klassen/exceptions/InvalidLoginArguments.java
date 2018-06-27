f53421c673ed412465b7ec67a416e20694390437
xerayx <39841313+xerayx@users.noreply.github.com>
2018-06-27 19:00:06 +0200

    Update InvalidLoginArguments.java

--- Klassen/exceptions/InvalidLoginArguments.java
+++ Klassen/exceptions/InvalidLoginArguments.java
@@ -1,9 +1,15 @@
 package methods.exceptions;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class InvalidLoginArguments extends Exception{
 
-    public InvalidLoginArguments(String message) {
-        super(message);
+    public InvalidLoginArguments() {
+        super("Invalid Login Arguments");
+        JFrame error = new JFrame("Invalid Login Arguments");
+        JOptionPane.showMessageDialog(error,
+        	    "Your username or password is incorrect.");
     }
 
 }
