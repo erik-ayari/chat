469d1cd7872ba9269e8c9ed0712cfeda17da0028
xerayx <39841313+xerayx@users.noreply.github.com>
2018-06-27 18:02:11 +0200

    Update Server.java

--- Klassen/Server/Server.java
+++ Klassen/Server/Server.java
@@ -12,11 +12,15 @@ public class Server {
 	
 	ArrayList<JPanel> panels;
 	int i;
+	
+	static int port;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					JFrame frame = new JFrame("Welcome!");
+					port = Integer.parseInt(JOptionPane.showInputDialog(frame, "Please enter a port."));
 					Server window = new Server();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
@@ -33,7 +37,7 @@ public class Server {
 	private void initialize() {
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		
-		frame = new JFrame();
+		frame = new JFrame("Server Administrator");
 		frame.setBounds(100, 100, screen.width/2, screen.height/2);
 		frame.setLocation(screen.width/2-frame.getSize().width/2, screen.height/2-frame.getSize().height/2);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
