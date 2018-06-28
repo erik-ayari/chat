a6d618e1f872a5f92e1cc28e953eb39b6b473749
xerayx <39841313+xerayx@users.noreply.github.com>
2018-06-28 17:04:18 +0200

    Update Server.java

--- Klassen/Server/Server.java
+++ Klassen/Server/Server.java
@@ -1,77 +1,68 @@
-package server;
+package Server;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.*;
+import methods.ReceiveMessages;
+import methods.ServerSession;
+import org.json.JSONException;
+
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Server {
-	
-	private JFrame frame;
-	JTabbedPane tabbedPane;
-	
-	ArrayList<JPanel> panels;
-	int i;
-	
-	static int port;
-	ServerSession serverSession;
-	String[] chatrooms;
-	ChatroomGUI cgi;
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					JFrame frame = new JFrame("Welcome!");
-					port = Integer.parseInt(JOptionPane.showInputDialog(frame, "Please enter a port."));
-					Server window = new Server();
-					window.frame.setVisible(true);
-					serverSession = new ServerSession(port);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    static int port;
+    ServerSession serverSession;
+    JTabbedPane tabbedPane;
+    ArrayList<JPanel> panels;
+    int i;
+    String[] chatrooms;
+    ChatroomGUI cgi;
+    private JFrame frame;
 
-	public Server() {
-		initialize();
-	}
-	
-	private void initialize() {
-		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-		
-		frame = new JFrame("Server Administrator");
-		frame.setBounds(100, 100, screen.width/2, screen.height/2);
-		frame.setLocation(screen.width/2-frame.getSize().width/2, screen.height/2-frame.getSize().height/2);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		panels = new ArrayList<JPanel>();
-		
-		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		
-		chatrooms[] = serverSession.getCurrentChatrooms();
-		cgi = new ChatroomGUI(panels);
-		cgi.addChatrooms(chatrooms[], tabbedPane);
-		
-		frame.setContentPane(tabbedPane);
-		
-		while(true) {
-			String[] input = serverSession.waitForMessages();
-			if(input[0] == "msgdistribute") {
-				String message = input[1];
-				String user = input[2];
-				String chatroom = input[3];
-				int index = cgi.findTabByName(chatroom, tabbedPane);
-				JTextArea chatHistory = cgi.getChatHistory(index);
-				chatHistory.append(user + ": " + message + "\n");
-			} else if(input[0] == "chatroomadded") {
-				String chatroom[] = input[1];
-			}
-		}
-	}
+    public Server() throws IOException, JSONException {
+        initialize();
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    JFrame frame = new JFrame("Welcome!");
+                    port = Integer.parseInt(JOptionPane.showInputDialog(frame, "Please enter a port."));
+                    Server window = new Server();
+                    window.frame.setVisible(true);
+                    //serverSession = new ServerSession(port);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void initialize() throws IOException, JSONException {
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+
+        frame = new JFrame("Server Administrator");
+        frame.setBounds(100, 100, screen.width / 2, screen.height / 2);
+        frame.setLocation(screen.width / 2 - frame.getSize().width / 2, screen.height / 2 - frame.getSize().height / 2);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panels = new ArrayList<JPanel>();
+
+        tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+        tabbedPane.setFont(new Font("Tahoma", Font.PLAIN, 40));
+
+        serverSession = new ServerSession(port);
+
+        chatrooms = serverSession.getCurrentChatrooms();
+        cgi = new ChatroomGUI(panels);
+        cgi.addChatrooms(chatrooms, tabbedPane);
+
+        frame.setContentPane(tabbedPane);
+
+        ReceiveMessages rmsg = new ReceiveMessages(tabbedPane, panels, serverSession, port);
+        rmsg.start();
+    }
 
 }
