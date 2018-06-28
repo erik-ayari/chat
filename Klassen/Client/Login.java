5301dc10441eff627d9893c6cbaafc6a8db4a1b4
xerayx <39841313+xerayx@users.noreply.github.com>
2018-06-28 17:12:15 +0200

    Update Login.java

--- Klassen/Client/Login.java
+++ Klassen/Client/Login.java
@@ -1,71 +1,52 @@
-package client;
+package Client;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Login {
 
-	JFrame frame;
-	JTextField txtServer, txtUsername, txtPassword, txtChatroom;
-	JButton btnLogIn;
+    JFrame frame;
+    JTextField txtServer, txtUsername, txtPassword;
+    JButton btnLogIn;
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login window = new Login();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public Login() {
+        initialize();
+    }
 
-	public Login() {
-		initialize();
-	}
+    private void initialize() {
+        frame = new JFrame();
+        frame.setResizable(false);
+        frame.setBounds(100, 100, 300, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
 
-	private void initialize() {
-		frame = new JFrame();
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 300, 500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
-		
-		JLabel lblLogIn = new JLabel("Log In");
-		lblLogIn.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		lblLogIn.setHorizontalAlignment(SwingConstants.CENTER);
-		frame.getContentPane().add(lblLogIn);
-		
-		txtServer = new JTextField();
-		txtServer.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		txtServer.setText("Server");
-		frame.getContentPane().add(txtServer);
-		txtServer.setColumns(10);
-		
-		txtUsername = new JTextField();
-		txtUsername.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		txtUsername.setText("Username");
-		frame.getContentPane().add(txtUsername);
-		txtUsername.setColumns(10);
-		
-		txtPassword = new JTextField();
-		txtPassword.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		txtPassword.setText("Password");
-		frame.getContentPane().add(txtPassword);
-		txtPassword.setColumns(10);
-		
-		txtChatroom = new JTextField();
-		txtChatroom.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		txtChatroom.setText("Chatroom");
-		frame.getContentPane().add(txtChatroom);
-		txtChatroom.setColumns(10);
-		
-		btnLogIn = new JButton("Enter");
-		btnLogIn.setFont(new Font("Tahoma", Font.PLAIN, 40));
-		frame.getContentPane().add(btnLogIn);
-		
-	}
+        JLabel lblLogIn = new JLabel("Log In");
+        lblLogIn.setFont(new Font("Tahoma", Font.PLAIN, 40));
+        lblLogIn.setHorizontalAlignment(SwingConstants.CENTER);
+        frame.getContentPane().add(lblLogIn);
+
+        txtServer = new JTextField();
+        txtServer.setFont(new Font("Tahoma", Font.PLAIN, 40));
+        txtServer.setText("Server");
+        frame.getContentPane().add(txtServer);
+        txtServer.setColumns(10);
+
+        txtUsername = new JTextField();
+        txtUsername.setFont(new Font("Tahoma", Font.PLAIN, 40));
+        txtUsername.setText("Username");
+        frame.getContentPane().add(txtUsername);
+        txtUsername.setColumns(10);
+
+        txtPassword = new JTextField();
+        txtPassword.setFont(new Font("Tahoma", Font.PLAIN, 40));
+        txtPassword.setText("Password");
+        frame.getContentPane().add(txtPassword);
+        txtPassword.setColumns(10);
+
+        btnLogIn = new JButton("Enter");
+        btnLogIn.setFont(new Font("Tahoma", Font.PLAIN, 40));
+        frame.getContentPane().add(btnLogIn);
+
+    }
 
 }
