39bddf69744c54a08cf75c5a077b6bc7482d80c7
xerayx <39841313+xerayx@users.noreply.github.com>
2018-06-28 17:05:47 +0200

    Update ChatroomGUI.java

--- Klassen/Server/ChatroomGUI.java
+++ Klassen/Server/ChatroomGUI.java
@@ -1,4 +1,4 @@
-package server;
+package Server;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
@@ -9,69 +9,77 @@ import java.util.*;
 import javax.swing.*;
 
 public class ChatroomGUI {
-	
-	ArrayList<JPanel> panels;
-	Font font = new Font("Tahoma", Font.PLAIN, 40);
-	
-	public ChatroomGUI(ArrayList<JPanel> panels) {
-		this.panels = panels;
-	}
-	
-	public JTextArea getChatHistory(int index) {
-		BorderLayout layout = (BorderLayout) panels.get(index).getLayout();
-		JTextArea chatHistory = (JTextArea) layout.getLayoutComponent(BorderLayout.CENTER);
-		return chatHistory;
-	}
-	
-	public JTextField getMessageField(int index) {
-		BorderLayout layout = (BorderLayout) panels.get(index).getLayout();
-		JTextField messageField = (JTextField) layout.getLayoutComponent(BorderLayout.PAGE_END);
-		return messageField;
-	}
-	
-	public int findTabByName(String title, JTabbedPane tabbedPane)  
-    {
-      int tabCount = tabbedPane.getTabCount();
-      for (int i=0; i < tabCount; i++) 
-      {
-        String tabTitle = tabbedPane.getTitleAt(i);
-        if (tabTitle.equals(title)) return i;
-      }
-      return -1;
-    }
-	
-	public void addChatrooms(String[] chatrooms, JTabbedPane tabbedPane) {
-		
-		
-		for(int i = 0; i < chatrooms.length; i++) {
-			JPanel panel = new JPanel(new BorderLayout());
-			JTextArea chatHistory = new JTextArea();
-			JTextField messageField = new JTextField();
-			JButton removeUser = new JButton();
-			removeUser.setFont(new Font("Tahoma", Font.PLAIN, 40));
-			removeUser.setText("Remove User");
-			removeUser.addActionListener(new AbstractAction() {
 
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// Remove User
-					
-				}
-				
-			});
-			try {
-				String localHost = InetAddress.getLocalHost().toString();
-				String lh[] = localHost.split("/");
-				chatHistory.append("Server started on " + lh[1]);
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			}
-			chatHistory.setFont(font);
-			chatHistory.setEditable(false);
-			panel.add(chatHistory, BorderLayout.CENTER);
-			panel.add(removeUser, BorderLayout.PAGE_END);
-			panels.add(panel);
-			tabbedPane.addTab(chatrooms[i], panels.get(i));
-		}
-	}
+    ArrayList<JPanel> panels;
+    Font font = new Font("Tahoma", Font.PLAIN, 40);
+
+    public ChatroomGUI(ArrayList<JPanel> panels) {
+        this.panels = panels;
+    }
+
+    public JTextArea getChatHistory(int index) {
+        BorderLayout layout = (BorderLayout) panels.get(index).getLayout();
+        JTextArea chatHistory = (JTextArea) layout.getLayoutComponent(BorderLayout.CENTER);
+        return chatHistory;
+    }
+
+    public JTextField getMessageField(int index) {
+        BorderLayout layout = (BorderLayout) panels.get(index).getLayout();
+        JTextField messageField = (JTextField) layout.getLayoutComponent(BorderLayout.PAGE_END);
+        return messageField;
+    }
+
+    public int findTabByName(String title, JTabbedPane tabbedPane)
+    {
+        int tabCount = tabbedPane.getTabCount();
+        for (int i=0; i < tabCount; i++)
+        {
+            String tabTitle = tabbedPane.getTitleAt(i);
+            if (tabTitle.equals(title)) return i;
+        }
+        return -1;
+    }
+
+    public void addChatrooms(String[] chatrooms, JTabbedPane tabbedPane) {
+
+
+        for(int i = 0; i < chatrooms.length; i++) {
+            JPanel panel = new JPanel(new BorderLayout());
+            JTextArea chatHistory = new JTextArea();
+            JTextField messageField = new JTextField();
+            JButton removeUser = new JButton();
+            removeUser.setFont(new Font("Tahoma", Font.PLAIN, 40));
+            removeUser.setText("Remove User");
+            removeUser.addActionListener(new AbstractAction() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // Remove User
+
+                }
+
+            });
+            try {
+                String localHost = InetAddress.getLocalHost().toString();
+                String lh[] = localHost.split("/");
+                chatHistory.append("Server started on " + lh[1]);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            chatHistory.setFont(font);
+            chatHistory.setEditable(false);
+            panel.add(chatHistory, BorderLayout.CENTER);
+            panel.add(removeUser, BorderLayout.PAGE_END);
+            panels.add(panel);
+            tabbedPane.addTab(chatrooms[i], panels.get(i));
+        }
+    }
+    
+    public void printInEveryChatroom(String message) {
+    	for(JPanel panel : panels) {
+    		BorderLayout layout = (BorderLayout) panel.getLayout();
+    		JTextArea chatHistory = (JTextArea) layout.getLayoutComponent(BorderLayout.CENTER);
+    		chatHistory.append(message + "\n");
+    	}
+    }
 }
