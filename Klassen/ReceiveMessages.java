e2d8fa7bd559cad216881af89a270a70f5ce4040
xerayx <39841313+xerayx@users.noreply.github.com>
2018-06-28 17:41:27 +0200

    Update ReceiveMessages.java

--- Klassen/ReceiveMessages.java
+++ Klassen/ReceiveMessages.java
@@ -1,67 +1,75 @@
 package methods;
 
-import client.ChatroomGUI;
-import client.Chat;
 
-import java.awt.Font;
+import Client.ChatroomGUI;
+
+import javax.swing.*;
+
+import java.awt.Color;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.text.AttributedString;
 import java.util.ArrayList;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
-import javax.swing.JTextArea;
 
-public class ReceiveMessages implements Runnable{
-	
-	JTabbedPane tabbedPane;
-	ArrayList<JPanel> panels;
-	int port;
-	String username;
-	ServerSocket serverSocket;
-	ObjectInputStream ois;
-	
-	public ReceiveMessages(JTabbedPane tabbedPane, ArrayList<JPanel> panels, int port, String username) {
-		this.tabbedPane = tabbedPane;
-		this.panels = panels;
-		this.port = port;
-		this.username = username;
-		try {
-			serverSocket = new ServerSocket(this.port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void run() {
-		ChatroomGUI cgi = new ChatroomGUI(panels);
-		while(true) {
-			try {
-				Socket socket = serverSocket.accept();
-				ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
-				String[] in = (String[]) inStream.readObject();
-				
-				serverSocket.close();
-				
-				if(in[0]=="msgdistribute") {
-					String message = in[1];
-					String user = in[2];
-					String chatroom = in[3];
-					int index = cgi.findTabByName(chatroom, tabbedPane);
-					JTextArea chatHistory = cgi.getChatHistory(index);
-					if(user!=username) {
-						chatHistory.append(user + ": " + message + "\n");
-					} else {
-						chatHistory.append("<b>" + user + ": " + message + "</b>");
-					}
-				} else if(in[0]=="chatroomadded") {
-					String chatroom = in[1];
-				}
-			} catch (IOException | ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+public class ReceiveMessages extends Thread {
+
+    JTabbedPane tabbedPane;
+    ArrayList<JPanel> panels;
+    int port;
+    String username;
+    ServerSocket serverSocket;
+    ObjectInputStream ois;
+
+    public ReceiveMessages(JTabbedPane tabbedPane, ArrayList<JPanel> panels, int port, String username) {
+        this.tabbedPane = tabbedPane;
+        this.panels = panels;
+        this.port = port;
+        this.username = username;
+//        try {
+//            //////////// KONSTANTER PORT FÃœR TESTEN
+//            //serverSocket = new ServerSocket(34568);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public void run() {
+        ChatroomGUI cgi = new ChatroomGUI(panels);
+        while (true) {
+            try {
+
+                serverSocket = new ServerSocket(55555);
+
+                Socket socket = serverSocket.accept();
+                ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+                String[] in = (String[]) inStream.readObject();
+
+                serverSocket.close();
+
+                if (in[0].equals("msgDistribute")) {
+                    String message = in[1];
+                    String user = in[2];
+                    String chatroom = in[3];
+
+                    System.out.println(message+user+chatroom);
+
+                    int index = cgi.findTabByName(chatroom, tabbedPane);
+                    JTextArea chatHistory = cgi.getChatHistory(index);
+                    if (user != username) {
+                        chatHistory.append(user + ": " + message + "\n");
+                    } else {
+                        chatHistory.append("<b>" + user + ": " + message + "</b>");
+                    }
+                    if(index != tabbedPane.getSelectedIndex()) {
+                    	tabbedPane.setForegroundAt(index, Color.YELLOW);
+                    }
+                } else if (in[0] == "chatroomadded") {
+                    String chatroom = in[1];
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
